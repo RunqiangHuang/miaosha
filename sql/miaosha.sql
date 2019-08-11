d2e54b955c8365f25cf1f8e6f8a8409f3ca55aa0
HuangRunQiang <644465093@qq.com>
2019-08-11 22:30:04 +0800

    增加sql语句

---
+++ sql/miaosha.sql
@@ -0,0 +1,140 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.7.20-log : Database - miaosha
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`miaosha` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `miaosha`;
+
+/*Table structure for table `item` */
+
+DROP TABLE IF EXISTS `item`;
+
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL DEFAULT '',
+  `price` double(10,0) NOT NULL DEFAULT '0',
+  `description` varchar(500) DEFAULT '',
+  `sales` int(11) NOT NULL DEFAULT '0',
+  `img_url` varchar(200) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `item` */
+
+insert  into `item`(`id`,`title`,`price`,`description`,`sales`,`img_url`) values (1,'11',11,'11',1,'111'),(2,'test',20,'123',2,'1212.img');
+
+/*Table structure for table `item_stock` */
+
+DROP TABLE IF EXISTS `item_stock`;
+
+CREATE TABLE `item_stock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stock` int(11) NOT NULL DEFAULT '0',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `item_stock` */
+
+insert  into `item_stock`(`id`,`stock`,`item_id`) values (1,49,1),(2,48,2);
+
+/*Table structure for table `order_info` */
+
+DROP TABLE IF EXISTS `order_info`;
+
+CREATE TABLE `order_info` (
+  `id` varchar(32) NOT NULL DEFAULT '',
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `item_price` double(10,2) NOT NULL DEFAULT '0.00',
+  `amount` int(11) NOT NULL DEFAULT '0',
+  `order_price` double(10,2) DEFAULT '0.00',
+  `promo_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `order_info` */
+
+insert  into `order_info`(`id`,`user_id`,`item_id`,`item_price`,`amount`,`order_price`,`promo_id`) values ('20190810200',6,2,0.00,1,0.00,NULL),('20190810300',6,1,11.00,1,11.00,NULL);
+
+/*Table structure for table `promo` */
+
+DROP TABLE IF EXISTS `promo`;
+
+CREATE TABLE `promo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `promo_name` varchar(64) NOT NULL DEFAULT '',
+  `start_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `end_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `promo_item_price` double(10,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `promo` */
+
+/*Table structure for table `sequence_info` */
+
+DROP TABLE IF EXISTS `sequence_info`;
+
+CREATE TABLE `sequence_info` (
+  `name` varchar(64) NOT NULL DEFAULT '',
+  `current_value` int(11) NOT NULL DEFAULT '0',
+  `step` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sequence_info` */
+
+insert  into `sequence_info`(`name`,`current_value`,`step`) values ('order_info',4,1);
+
+/*Table structure for table `user_info` */
+
+DROP TABLE IF EXISTS `user_info`;
+
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL DEFAULT '',
+  `gender` tinyint(4) NOT NULL DEFAULT '0' COMMENT '1: 男, 2: 女',
+  `age` int(11) NOT NULL DEFAULT '0',
+  `telphone` varchar(16) NOT NULL DEFAULT '',
+  `register_mode` varchar(64) NOT NULL DEFAULT '' COMMENT 'byphone, bywechat, byalipay',
+  `third_party_id` varchar(64) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `telphone` (`telphone`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_info` */
+
+insert  into `user_info`(`id`,`name`,`gender`,`age`,`telphone`,`register_mode`,`third_party_id`) values (1,'测试',1,20,'121212','0','1'),(5,'tt',1,2,'111','byPhone',''),(6,'admin',1,20,'123456','byPhone','');
+
+/*Table structure for table `user_password` */
+
+DROP TABLE IF EXISTS `user_password`;
+
+CREATE TABLE `user_password` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `encrpt_password` varchar(128) NOT NULL DEFAULT '',
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_password` */
+
+insert  into `user_password`(`id`,`encrpt_password`,`user_id`) values (1,'123456',1),(4,'4QrcOUm6Wau+VuBX8g+IPg==',5),(5,'4QrcOUm6Wau+VuBX8g+IPg==',6);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
